ddd7d115aa5d8c31cbebd33244723d52594dd8b4
Chau Ying <chau-ying-kot@hotmail.com>
2023-12-17 18:13:43 +0100

    Refine the dataset, less smoothing on the energy

--- src/data/LSTM_dataset.py
+++ src/data/LSTM_dataset.py
@@ -1,6 +1,7 @@
 import torch
 from tqdm.autonotebook import tqdm
 from torch.utils.data import Dataset
+import torch.fft as fft
 import torch.nn.functional as F
 import glob
 import pickle
@@ -14,7 +15,7 @@ from itertools import groupby
 import matplotlib.pyplot as plt
 
 class LSTMDataset(Dataset):
-    def __init__(self, data_path, labels_path,  features_selection, transform=None, max_length = None):
+    def __init__(self, data_path, labels_path,  features_selection = None, transform=None, max_length = None):
         """
         param:
             - features_selection : select which features to used for the training, 0 is the max_energy 1 to 6 is the mode
@@ -27,6 +28,7 @@ class LSTMDataset(Dataset):
         
         self.transform = transform
         self.max_length = max_length
+ 
         self.features_selection = features_selection
 
         self.inputs = []
@@ -35,10 +37,11 @@ class LSTMDataset(Dataset):
         with tqdm(self.all_shots) as pbar:
             pbar.set_description('data processing')
             for shotno in pbar :
-                data = self.load_shot(shotno)
-                features = self.process_data(data)
+                data_features = self.load_shot(shotno)
+                data_label = self.load_label(shotno)
 
-                label = self.process_labels(shotno, torch.tensor(data['x']['spectrogram']['time']))
+                features = self.process_data(data_features, data_label['time'][0])
+                label = self.process_labels(shotno, torch.tensor(data_features['x']['spectrogram']['time']))
 
                 self.inputs.append(features)
                 self.labels.append(label)
@@ -48,15 +51,22 @@ class LSTMDataset(Dataset):
         return len(self.all_shots)
 
     def __getitem__(self, idx):
-        input = self.inputs[idx][:, self.features_selection]
-        if len(input.shape) == 1:
-            input = input.unsqueeze(-1)
+        if self.features_selection is not None:
+            input = [self.inputs[idx][key].numpy() for key in self.features_selection]
+        else:
+            input = [self.inputs[idx][key].numpy() for key in self.inputs[idx].keys()]
 
-        return input, self.labels[idx]
-        # return self.inputs[idx][:,0].unsqueeze(-1), self.labels[idx]
+        return torch.tensor(input, dtype=torch.float32).swapaxes(0,1), self.labels[idx]
+
     def load_shot(self, shotno):
         with open(os.path.join(self.data_path, f"{shotno}.pickle"), "rb") as f:
             return pickle.load(f)
+        
+    def load_label(self, shotno):
+        with open(os.path.join(self.labels_path, f'TCV_{shotno}_apau_MHD_labeled.csv')) as f:
+            data = np.loadtxt(f, skiprows=1, delimiter=',')
+            return {'time' : data[:, 0],
+                    'label' : data[:, 4]}
     
     # def compute_labels(self, max_energies, threshold=0.8):
     #     """
@@ -84,17 +94,25 @@ class LSTMDataset(Dataset):
 
         spec_odd = (spec_odd - mean) / std
 
-        max_energies, _ = torch.max(torch.from_numpy(gaussian_filter(spec_odd, 10)).squeeze(), dim = 1)
-        max = torch.max(max_energies)
-        max_energies = torch.from_numpy(gaussian_filter(max_energies / max, 10)).squeeze()
+        # filtered = torch.from_numpy(gaussian_filter(spec_odd, 10)).squeeze()
+        # max_energies, _ = torch.max(filtered, dim = 1)
+        # freq_position = torch.argmax(filtered, dim=1)
+        # max = torch.max(max_energies)
+        # max_energies = torch.from_numpy(gaussian_filter(max_energies / max, 10)).squeeze()
 
-        return max_energies
+        max_energies, _ = torch.max(spec_odd, dim = 1)
+        freq_position = torch.argmax(spec_odd, dim=1)
+        max = torch.max(max_energies)
+        max_energies = max_energies / max
+        
+        return torch.from_numpy(gaussian_filter(max_energies, 1)), freq_position
     
     def compute_mode(self, mode, size) : 
         """
             Process the mode value, normalize and scale the value to have value between [0,1]
         """        
         mode = self.normalize_mode(mode)
+        mode = mode - mode.min()
         mode = mode / mode.max()
         mode = F.interpolate(mode.unsqueeze(0).unsqueeze(0), size=size).squeeze()
 
@@ -105,7 +123,7 @@ class LSTMDataset(Dataset):
 
         return (mode_val - mean) / std
     
-    def process_data(self, data) : 
+    def process_data(self, data, label_start) : 
         """
             Process the data
 
@@ -115,34 +133,46 @@ class LSTMDataset(Dataset):
         """
 
         spec_odd =  data['x']['spectrogram']['OddN']
+        spec_time = data['x']['spectrogram']['time']
+
+        spec_odd = spec_odd[spec_time >= label_start, :] # Keep only those that have a label
 
         if self.transform != None:
             spec_odd = self.transform(spec_odd)
 
-        max_energies = self.compute_max_energy(spec_odd)
+        max_energies, position = self.compute_max_energy(spec_odd)
+
+        input = {}
+
+        input['max_energies'] = max_energies
+        input['position'] = position
 
         # labels = self.process_labels()
 
         modes = data['y']['modes']
-        input = max_energies.float()
 
         for i in range(5):
             mode = torch.from_numpy(modes[f'N{i}'])
             mode[torch.isnan(mode)] = 0
             mode = self.compute_mode(mode, len(max_energies))
 
-            input = torch.vstack((input, mode.float()))
+            input[f'N{i}'] = mode
 
-        input = input.swapaxes(0,1)
+        mode = torch.from_numpy(modes['LM'])
+        mode[torch.isnan(mode)] = 0
+        mode = self.compute_mode(mode, len(max_energies))
+
+        input['LM'] = mode
 
         if self.max_length != None:
-            if len(input) > self.max_length:
-                # labels = labels[:self.max_length]
-                input = input[:self.max_length, :]
+            if len(input['max_energies']) > self.max_length:
+                for k, v in input.items():
+                    input[k] = v[:self.max_length]
             elif len(input) < self.max_length:
-                temp = torch.zeros((self.max_length, input.size(1)))
-                temp[:input.size(0), :] = input
-                input = temp
+                for k, v in input.items():
+                    temp = torch.zeros(self.max_length)
+                    temp[:v.size(0)] = v
+                    input[k] = temp
 
                 # temp = torch.zeros(self.max_length)
                 # temp[:labels.size(0)] = labels
@@ -182,3 +212,12 @@ class LSTMDataset(Dataset):
             true_labels = temp
 
         return true_labels
+    
+    def compute_pos_weight(self):
+        size = 0
+        nb_pos = 0
+        for l in self.labels:
+            size += len(l)
+            nb_pos += torch.sum(l == 1)
+        return size / nb_pos
+
