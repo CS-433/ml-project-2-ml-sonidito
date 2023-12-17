70e60e5e7cc8270f82275a789e7df93143e026e5
Chau Ying <chau-ying-kot@hotmail.com>
2023-12-17 18:18:13 +0100

    Use score instead of the loss

--- src/models/early_stopping.py
+++ src/models/early_stopping.py
@@ -1,21 +1,21 @@
 import torch
 class EarlyStopping:
 
-    def __init__(self, save_path, patience=3, delta=1e-3, ):
+    def __init__(self, save_path, patience=3, delta=1e-3,):
         self.patience = patience
         self.delta = delta
 
         self.counter = 0
-        self.min_loss = float('inf')
+        self.min_score = float('inf')
 
         self.save_path = save_path
 
-    def early_stop(self, loss, model):
-        if loss < self.min_loss:
-            self.min_loss = loss
+    def early_stop(self, score, model):
+        if score < self.min_score:
+            self.min_score = score
             self.counter = 0
             torch.save(model, self.save_path)
-        elif loss - self.min_loss > self.delta:
+        elif score - self.min_score > self.delta:
             self.counter += 1
             if self.counter >= self.patience:
                 return True
